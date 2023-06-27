b3a65da909e84985d355061437c51ec5e1cda3a6
Sundram <101039005+xyz-sundram@users.noreply.github.com>
2023-06-27 12:50:42 +0530

    Update config.mk

--- config.mk
+++ config.mk
@@ -15,9 +15,6 @@
 VENDOR_PATH := vendor/MiuiCamera
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib,system/lib)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(VENDOR_PATH)/system/lib64,system/lib64)
 
 PRODUCT_PACKAGES += \
